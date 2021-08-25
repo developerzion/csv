dc2746976bfda12027b4fb4b379f17457433731a
Moses Samuel <devparse.official@gmail.com>
2021-08-25 06:21:48 +0100

    Add to production

---
+++ db/workspace.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 25, 2021 at 07:04 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `workspace`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `surname` varchar(200) NOT NULL,
+  `age` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -2,21 +2,33 @@
 <!DOCTYPE html>
 <html>
   <head>      
-      <title>Devparse</title>
+      <title>Upload CSV Files Into MYSQL Database Using PHP</title>
       <link rel="shortcut icon" href="logo.png">
-      <link rel="stylesheet" type="text/css" href="style.css">
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+      <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+      <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
   </head>
 
 <body>
   <div class="navigation">
-    <span><img src="logo.png" class="logo"></span>
+    <span><img src="img/logo.png" class="logo"></span>
     <span>How to Upload CSV Files Into MYSQL Database Using PHP</span>
   </div>
 
   <?php
-    $conn = mysqli_connect("localhost","root", "");
-    mysqli_select_db($conn, "workspace");
+    //DEVELOPEMENT
+    $conn = mysqli_connect("localhost","root", "","workspace");
 
+    //PRODUCTION
+    $conn = mysqli_connect("localhost","root", "","workspace");
+
+
+    function message($msg, $status){
+      return "<div class='col-lg-5 col-lg-offset-3'><div class='alert alert-$status'>$msg</div></div>";
+    }
+
+    //Upload the csv file into the database
     if(isset($_POST['uploadfile'])){
       $csvTemp = $_FILES['csvfile']['tmp_name'];
       $getContent = file($csvTemp);
@@ -35,17 +47,68 @@
       }
 
       if($result){
-        echo "Records successfully inserted";
+        echo message("Records successfully inserted",'success');
       }else{
-        echo "Error occured while uploading file";
+        echo message("Error occured while uploading file",'danger');
+      }
+    }
+
+    //Delete record from the database
+    if(isset($_POST['trash'])){
+      $id = $_POST['trashid'];
+      $chkTrash = mysqli_query($conn, "DELETE FROM tbl_users WHERE `id`='$id'");
+      if($chkTrash){
+        echo message("Records successfully deleted",'success');
       }
     }
   ?>
 
-  <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data">
-    <input type="file" name="csvfile" required>
-    <button class="btn" name="uploadfile">Click to upload CSV file</button>
-  </form>
+<div class="row">
+    <div class="col-lg-5 col-lg-offset-3">
+      <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data">
+        <table>
+          <tr>
+            <td><input type="file" class="form-control" name="csvfile" required></td><td><button class="btn btn-success" name="uploadfile">Click to upload CSV file</button></td>
+          </tr>
+        </table>
+      </form>
+      <br>
+      <h2>Records</h2>
+      <table class="table table-bordered table-hover">
+          <tr>
+            <td>S/N</td>
+            <td>Firstname</td>
+            <td>Lastname</td>
+            <td>Age</td>
+            <td>Action</td>
+          </tr>
+          <?php
+            $chkTable = mysqli_query($conn, "SELECT * FROM tbl_users");
+            if(mysqli_num_rows($chkTable) < 1){
+              echo "<tr><td colspan='5' style='text-align:center'>No record</td></tr>";
+            }else{
+              $count = 1;
+              while($row = mysqli_fetch_array($chkTable)){ ?>
+                <form action="" method="POST">
+                  <input type="text" name="trashid" value="<?php echo $row['id']; ?>" hidden>
+                  <tr>
+                    <td><?php echo $count++; ?></td>
+                    <td><?php echo $row['firstname']; ?></td>
+                    <td><?php echo $row['surname']; ?></td>
+                    <td><?php echo $row['age']; ?></td>
+                    <td><button class="btn btn-danger" name="trash"><span class="fa fa-trash"></span></button></td>
+                  </tr>
+                </form>
+                <?php
+
+              }
+            }
+          ?>
+      </table>
+<a target="_blank" href="https://github.com/developerzion"><span class="fa fa-github"></span> Github</a> | <a target="_blank" href="https://www.youtube.com/channel/UCe0t7d9o4kd9gc9_sdUL_TA"><span class="fa fa-youtube"></span> Youtube Channel</a>
+
+    </div>
+  </div>
 </body>
 
 </html>
